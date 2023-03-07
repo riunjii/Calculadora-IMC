7c7f7144a91b311e54b9b0225da3eccfe365b33d
riunjii <vitor2002_souza@hotmail.com>
2023-03-07 11:03:17 -0300

    2°

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function calcular(){
-    var peso, altura, resp
+    var peso, altura, resp, status;
 
     peso = parseFloat(document.getElementById("peso").value);
     altura = parseFloat(document.getElementById("altura").value);
 
     resp = peso / Math.pow(altura,2);
 
-    document.getElementById("resp").value = resp;
+    document.getElementById("resp").value = resp.toFixed(2);
 
+    if(resp)
 
 }
