5e467525993a7ec7d5ff4db2ab23cfdb1783877d
riunjii <vitor2002_souza@hotmail.com>
2023-03-22 17:46:57 -0300

    updt

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function calcular(){
     var peso, altura, resp, status;
 
     peso = parseFloat(document.getElementById("peso").value);
+
+    //importante dividir o valor por 100, para converter cm em metros
     altura = parseFloat(document.getElementById("altura").value) / 100;
 
     resp = peso / Math.pow(altura,2);
