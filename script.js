a86afc1b290ec58b5f823216b2daa235f5e4d926
riunjii <vitor2002_souza@hotmail.com>
2023-03-22 16:07:24 -0300

    funcional

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+
 function calcular(){
     var peso, altura, resp, status;
 
     peso = parseFloat(document.getElementById("peso").value);
-    altura = parseFloat(document.getElementById("altura").value);
+    altura = parseFloat(document.getElementById("altura").value) / 100;
 
     resp = peso / Math.pow(altura,2);
 
     document.getElementById("resp").value = resp.toFixed(2);
-
-    if(resp)
 
 }
